238de187a8c3ebd5bdb3dc0bf2277979cd6c8cb3
teoman <kerteo.22@robcol.k12.tr>
2021-11-15 02:48:07 +0300

    Using Objects 1

---
+++ Using Objects/2.1/Game Skeleton.java
@@ -0,0 +1,13 @@
+public class ConsoleGame
+{
+
+    private int healthAmount;
+    private int score;
+    private int lifeAmount;
+    private String characterName;
+    private String npcName;
+    private String characterName;
+
+
+
+}
---
+++ Using Objects/2.1/Pizza Instance Variables.java
@@ -0,0 +1,12 @@
+public class PizzaTester
+{
+    private int amount;
+    private char size;
+    private String type;
+    public static void main(String[] args)
+    {
+        System.out.println("You should be able to run this");
+        System.out.println("if you added the instance variables correctly");
+
+    }
+}
