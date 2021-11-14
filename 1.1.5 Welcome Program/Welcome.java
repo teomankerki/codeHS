a91421d35ea3bfc1f6a5ef5a93aedeb44bdc6fde
teoman <kerteo.22@robcol.k12.tr>
2021-11-15 02:32:10 +0300

    Stashed Changes

---
+++ 1.1.5 Welcome Program/Welcome.java
@@ -0,0 +1,9 @@
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+        
+        System.out.println("My name is name!");
+        System.out.println("My favorite hobby is playing the guitar");
+    } 
+}
