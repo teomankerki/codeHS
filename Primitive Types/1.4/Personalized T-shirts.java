418076ff2252a5440658721466389242638518a5
teoman <kerteo.22@robcol.k12.tr>
2021-11-15 02:29:29 +0300

    Primitive Types

---
+++ Primitive Types/1.4/Personalized T-shirts.java
@@ -0,0 +1,15 @@
+public class Tshirt
+{
+    public static void main(String[] args)
+    {
+        int cost = 22;
+        System.out.println("The t-shirt costs $" + cost + ".");
+        cost++;
+        System.out.println("A personalized t-shirt costs $" + (cost) + ".");
+        cost--;
+        System.out.println("A personalized t-shirt costs $" + (cost) + ".");
+
+
+
+    }
+}
